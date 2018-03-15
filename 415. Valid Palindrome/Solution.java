3d27db4e8f09ca451d9908f59451c56eb9eb293d
yh83752n <huang.yulin@outlook.com>
2018-03-15 15:56:58 -0400

    Update Solution.java

--- 415. Valid Palindrome/Solution.java
+++ 415. Valid Palindrome/Solution.java
@@ -3,18 +3,21 @@ class Solution {
 		if (s == null || s.length() == 0) {
 			return true;
 		}
+		
 		int front = 0;
 		int end = s.length() - 1;
 		while (front < end) {
 			while (front < s.length() && !isValid(s.charAt(front))) {
 				front++;
 			}
+			
 			if (front == s.length()) {
 				return true;
 			}
 			while (end >= 0 && !isValid(s.charAt(end))) {
 				end--;
 			}
+			
 			if (Character.toLowerCase(front) != Character.toLowerCase(end)) {
 				break;
 			} else {
@@ -22,6 +25,7 @@ class Solution {
 				end--;
 			}
 		}
+		
 		return end <= front;
 	}
 
