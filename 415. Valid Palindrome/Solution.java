34f36aa3033652b6a68dff9a40b3b06f9d118f2f
yh83752n <huang.yulin@outlook.com>
2018-03-15 15:52:02 -0400

    Update Solution.java

--- 415. Valid Palindrome/Solution.java
+++ 415. Valid Palindrome/Solution.java
@@ -1,30 +1,31 @@
-public class Solution {
-    public boolean isPalindrome(String s) {
-        if(s==null||s.length()==0){
-            return true;
-        }
-        int front=0;
-        int end=s.length()-1;
-        while(front<end){
-            while(front<s.length() && !isValid(s.charAt(front))){
-                front++;
-            }
-            if(front==s.length()){
-                return true;
-            }
-            while(end>=0&&!isValid(s.charAt(end))){
-                end--;
-            }
-            if(Character.toLowerCase(front)!=Character.toLowerCase(end)){
-                break;
-            }else{
-                front++;
-                end--;
-            }
-        }
-        return end<=front;
-    }
-    private boolean isValid(char c){
-        return Character.isLetter(c)||Character.isDigit(c);
-    }
+class Solution {
+	public boolean isPalindrome(String s) {
+		if (s == null || s.length() == 0) {
+			return true;
+		}
+		int front = 0;
+		int end = s.length() - 1;
+		while (front < end) {
+			while (front < s.length() && !isValid(s.charAt(front))) {
+				front++;
+			}
+			if (front == s.length()) {
+				return true;
+			}
+			while (end >= 0 && !isValid(s.charAt(end))) {
+				end--;
+			}
+			if (Character.toLowerCase(front) != Character.toLowerCase(end)) {
+				break;
+			} else {
+				front++;
+				end--;
+			}
+		}
+		return end <= front;
+	}
+
+	private boolean isValid(char c) {
+		return Character.isLetter(c) || Character.isDigit(c);
+	}
 }
